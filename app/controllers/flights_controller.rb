6a3ae7334ab36b25aa017872f4d763f1ad785593
bchalman <blakechalman@gmail.com>
2019-01-14 22:54:17 -0800

    Add select flight logic

--- app/controllers/flights_controller.rb
+++ app/controllers/flights_controller.rb
@@ -7,12 +7,21 @@ class FlightsController < ApplicationController
     @num_passengers = [1,2,3,4].map{|num| [ pluralize(num, 'person'), num ]}
     @departure_time_options = Flight.select(:departure_time).group('DATE(departure_time)')
                                     .map{ |f| [ f.date_formatted, f.departure_time.to_date ]}
+
+    @queried_flights = Flight.all
+
+    if !params[:departure_airport_id].nil?
+      @queried_flights = @queried_flights.where(departure_airport_id: params[:departure_airport_id])
+      @queried_flights = @queried_flights.where(arrival_airport_id: params[:arrival_airport_id])
+      @queried_flights = @queried_flights.where("DATE(departure_time) = ?", params[:departure_time].to_date)
+    end
+
   end
 
 
   private
 
     def flight_params
-      params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :departure_time, :duration, :num_passengers)
+      params.require(:flight).permit(:departure_airport_id, :arrival_airport_id, :departure_time, :num_passengers)
     end
 end
